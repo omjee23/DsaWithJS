2eb1dcf792705ca2dcc9eaba967bd538d25960ca
Om Jee Pal <om22@navgurukul.org>
2025-01-23 21:00:35 +0530

    modified array

--- mergArrary.js
+++ mergArrary.js
@@ -1,5 +1,5 @@
 let data1 = [33, 45, 78, 77, 90, 53, 43];
-let data2 = [95, 57, 13, 23, 59, 70, 76];
+let data2 = [95, 57, 13, 23, 59, 70, 76 ,78,90,60];
 let data3 = [];
 for (let i = 0; i < data1.length; i++) {
   data3[i] = data1[i];
@@ -7,7 +7,7 @@ for (let i = 0; i < data1.length; i++) {
 for (let i = 0; i < data2.length; i++) {
   data3[data1.length + i] = data2[i];
 }
-console.log(data3);
+// console.log(data3);
 
 // default function to merge arrary
 let data4 = [...data1, ...data2];
@@ -15,4 +15,9 @@ console.log(data4);
 // default function to reverse array
 
 data4.reverse();
-console.log(data4);
+// console.log(data4);
+
+
+
+
+
